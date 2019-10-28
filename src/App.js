a8494f72e7d1b456502d0a825bfcaf2b8e7bdece
mateuszbukowski93 <mateuszbukowski93@gmail.com>
2019-10-28 09:06:53 +0100

    Created with user story. Need to be refactotred

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import QuotesMachine from "./compontets/QuotesMachine";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <QuotesMachine />
+      </div>
+    );
+  }
+}
+
+export default App;
